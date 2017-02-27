966a5dad6484101881b8dc891fa7b72472bfffbf
MJRT <majinhao@outlook.com>
2017-02-27 18:06:27 +0800

    up

--- api-gateway/src/main/java/tech/jinhaoma/InterceptorConfiguration.java
+++ api-gateway/src/main/java/tech/jinhaoma/InterceptorConfiguration.java
@@ -9,15 +9,15 @@ import tech.jinhaoma.check.AuthorizationInterceptor;
 /**
  * Created by mjrt on 2/22/2017.
  */
-@Configuration
-public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 注册拦截器
-        InterceptorRegistration ir = registry.addInterceptor(new AuthorizationInterceptor());
-        // 配置拦截的路径
-        ir.addPathPatterns("/**");
-        // 配置不拦截的路径
-        ir.excludePathPatterns("/login","/404","/index.html");
-    }
-}
+//@Configuration
+//public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 注册拦截器
+//        InterceptorRegistration ir = registry.addInterceptor(new AuthorizationInterceptor());
+//        // 配置拦截的路径
+//        ir.addPathPatterns("/**");
+//        // 配置不拦截的路径
+//        ir.excludePathPatterns("/login","/404","/index.html");
+//    }
+//}
